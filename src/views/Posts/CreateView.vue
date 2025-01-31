1319c992a1c0a35265418c89aedd76fa836509f1
S0ngyoungjun <clesaaaaaa@gmail.com>
2025-02-01 00:01:26 +0900

    31

--- src/views/Posts/CreateView.vue
+++ src/views/Posts/CreateView.vue
@@ -3,5 +3,17 @@
 <template>
   <main>
      <h1 class="title">새 글 생성</h1>
+
+     <form>
+      <div>
+        <input type="text" placeholder="제목"/>
+      </div>
+
+      <div>
+        <textarea rows="7" placeholder="내용"></textarea>
+      </div>
+
+      <button class="primary-btn">생성</button>
+     </form>
   </main>
 </template>
